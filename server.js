e8f24edcf0aadc6585804da9470b55b1e9591f5b
AyemDan <danielMMomodu@gmail.com>
2024-12-10 20:56:34 +0100

    task2: First API

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-const express = require('express')
+import express from 'express';
+import router from './routes/index';
+
 const app = express()
 
+const port = process.env.PORT || 3000;
+
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
